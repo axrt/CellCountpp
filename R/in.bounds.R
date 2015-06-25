c02787cf5ea2d49898bb4e571ee9b85f53b07946
alext <alexander.tuzhikov@gmail.com>
2015-06-25 11:27:49 -0400

    in.bounds documented

--- R/in.bounds.R
+++ R/in.bounds.R
@@ -1,3 +1,20 @@
+#' Use to check whether the given value falls inbetween the bounds.
+#' 
+#' @param \code{x} numeric value
+#' @param \code{leftBound} left numeric value
+#' @param \code{rightBound} right numeric value
+#' @param \code{inclusive} should the comparison be inclusive (TRUE) or exclusive (FALSE)
+#' @return whether or not the x value falls inbetween leftBound and rightBound
+#' @examples
+#' x<-2.7
+#' leftBound<-1.7
+#' rightBound<-3.14
+#' in.bounds(x,leftBound,rightBound,inclusive=TRUE)
+#' 
 in.bounds<-function(x,leftBound, rightBound, inclusive=FALSE){
-  return(.Call("inBounds", x, leftBound, rightBound, inclusive))
+  if(inclusive){
+    return(x>=leftBound&&x<=rightBound)
+  }else{
+    return(x>leftBound&&x<rightBound)
+  }
 }
