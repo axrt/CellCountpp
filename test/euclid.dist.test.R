283ae67fba746856a606a20d796f09d1258ddf60
alext <alexander.tuzhikov@gmail.com>
2015-06-24 22:27:13 -0400

    Test on euclid.distance improved

--- test/euclid.dist.test.R
+++ test/euclid.dist.test.R
@@ -5,3 +5,12 @@ dist(rbind(a,b))
 
 euclid.dist(rep(1,5),rep(1,6))
 euclid.dist(rep("a",4),rep("b",4))
+
+mini.df<-rbind(a,b)
+system.time(sapply(1:1e5,function(i){
+  return(dist(mini.df))
+}))
+
+system.time(sapply(1:1e5,function(i){
+  return(euclid.dist(a,b))
+}))
