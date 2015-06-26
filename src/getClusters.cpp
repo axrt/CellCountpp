45fb0c1f3de27db7a0274be6c2a4e6fe7a35b1d0
alext <alexander.tuzhikov@gmail.com>
2015-06-25 23:55:18 -0400

    allChecks funcition implemented

--- src/getClusters.cpp
+++ src/getClusters.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include <stdio.h>
 #include <math.h> 
 
 using namespace Rcpp;
@@ -24,33 +23,41 @@ inline bool brightEnough(const NumericMatrix &img, const IntegerMatrix::Row &xys
   return(img(xys_i[0],xys_i[1])>0);
 }
 
-inline bool allChecks(const NumericMatrix &img, LogicalMatrix &path, const IntegerMatrix::Row &xys_i,
-                               const IntegerMatrix::Row &start, const double width){
-   const bool in_field=inField(img,xys_i);
-   const bool has_been_visited=hasBeenVisited(path,xys_i);
-                                 
+inline bool closeEnough(const IntegerMatrix::Row &xys_i,const IntegerMatrix::Row &start, 
+                                 const double width, const double var){
+  const double dist=sqrt(pow(xys_i[0]-start[0],2)+pow(xys_i[1]-start[1],2));
+  const double dist_var=dist+var;
+  return(dist<=width+var);
 }
 
-inline bool closeEnough(const IntegerMatrix::Row &xys_i,const IntegerMatrix::Row &start, const double width){
-  const double dist=sqrt(pow(xys_i[0]-start[0],2)+pow(xys_i[1]-start[1],2));
-  return(dist<=width);
+inline bool allChecks(const NumericMatrix &img, LogicalMatrix &path, const IntegerMatrix::Row &xys_i,
+                               const IntegerMatrix::Row &start, const double width, const double var){
+   const bool in_field=inField(img,xys_i);
+   const bool has_been_visited=hasBeenVisited(path,xys_i);
+   const bool bright_enough=brightEnough(img, xys_i);
+   const bool close_enough=closeEnough(xys_i, start, width, var);
+    
+    return(in_field&&!has_been_visited&&bright_enough&&closeEnough);
 }
 
 IntegerMatrix * checkNeighborhood(LogicalMatrix &path, IntegerMatrix::Row &xys_i, 
-                                     NumericMatrix &output, IntegerMatrix::Row &startPoint, const double width){
+                                     NumericMatrix &output, IntegerMatrix::Row &startPoint, 
+                                     const double width, const double var){
   //check if an adjasent pixel,is withing the view field, has been visited, is bright enough and is within the possible range
   
   return new IntegerMatrix();
 }
 
 // [[Rcpp::export]]
-RcppExport SEXP getClusters(SEXP imgMtx, SEXP sortedXY, SEXP pathMtx, SEXP meanWidth) {
+RcppExport SEXP getClusters(SEXP imgMtx, SEXP sortedXY, SEXP pathMtx, SEXP meanWidth, SEXP varWidth) {
    
    const NumericMatrix img(imgMtx);
          IntegerMatrix xys(sortedXY);
          LogicalMatrix path(pathMtx);
    const NumericVector widthV(meanWidth);
+   const NumericVector varV(varWidth);
    const double width=widthV[0];
+   const double var=varV[0];
    
    for(int i=0; i<xys.ncol();i++){
      //check if the point has been visited
