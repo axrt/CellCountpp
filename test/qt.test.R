6ff68f19dd3319563fba0ff5e2e7643305aa0a0e
axrt <alexander.tuzhikov@gmail.com>
2015-07-10 19:44:26 -0400

    latest

--- test/qt.test.R
+++ test/qt.test.R
@@ -14,5 +14,17 @@ label$pixmap<-Qt$QPixmap("inst/extradata/ischemia_sample.png")
 
 scroll.area$setWidget(label)
 
-window$setVisible(TRUE)
 
+
+zoom.image<-function(x=2.0){
+  
+  label$resize(x*label$pixmap$size())
+  update.scroll.bar<-function(scbr){
+    scbr$value <- x*sb$value+(x-1)*sb*sb$pageStep/2
+  }
+  update.scroll.bar(scroll.area$horizontalScrollBar()) 
+  update.scroll.bar(scroll.area$verticalScrollBar()) 
+}
+
+qconnect(x=label, signal="clicked",handler = function(i){print(i)})
+window$setVisible(TRUE)
